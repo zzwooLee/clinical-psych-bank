7020f4dfd273aa8551b639f5170ce6146a7da886
zzwooLee <pmaker62@gmail.com>
2026-04-19 13:37:59 +0900

    Update years.js

--- api/years.js
+++ api/years.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 // [#6 수정] Service Role Key
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.SUPABASE_KEY
 );
 
 /*
